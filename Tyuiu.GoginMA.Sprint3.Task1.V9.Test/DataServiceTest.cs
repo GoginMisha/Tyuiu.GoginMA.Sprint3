e6e1c37cfbec7cfc4e2cfd05850c119752dd62ff
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-10-22 17:00:28 +0500

    Task1

---
+++ Tyuiu.GoginMA.Sprint3.Task1.V9.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GoginMA.Sprint3.Task1.V9.Lib;
+
+namespace Tyuiu.GoginMA.Sprint3.Task1.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultSeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 4;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 5815.752;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GoginMA.Sprint3.Task1.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint3.Task1.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint3.Task1.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5039560e-90fd-4231-8ea2-f121b0fd3ce7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
