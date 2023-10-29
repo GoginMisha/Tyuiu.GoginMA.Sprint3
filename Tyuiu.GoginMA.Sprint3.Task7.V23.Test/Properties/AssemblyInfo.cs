5d262f996ec46a1bca78ae1f118d866edb698058
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-10-29 18:56:29 +0500

    Task7

---
+++ Tyuiu.GoginMA.Sprint3.Task7.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoginMA.Sprint3.Task7.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoginMA.Sprint3.Task7.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cdd6afd8-d9f9-4933-83e7-6ae2debfaa2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GoginMA.Sprint3.Task7.V23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GoginMA.Sprint3.Task7.V23.Lib;
+
+namespace Tyuiu.GoginMA.Sprint3.Task7.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                             *");
+            Console.WriteLine("* Вариант #23                                                            *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:          *");
+            Console.WriteLine("* f(x) на заданном диапазоне [-5; 5] с шагом 1. При делении на 0         *");
+            Console.WriteLine("* вернуть значение 0. Значение округлить до 2 знаков после запятой.      *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     +   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |   {1,5:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
